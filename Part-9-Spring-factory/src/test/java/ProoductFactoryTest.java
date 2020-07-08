09b1a03cbf849219d59947eeb868c55cb2e0f3a7
lkyi <lkyi@senthink.com>
2020-07-08 10:02:09 +0800

    添加factorybean相关内容

--- Part-9-Spring-factory/src/test/java/ProoductFactoryTest.java
+++ Part-9-Spring-factory/src/test/java/ProoductFactoryTest.java
@@ -5,9 +5,11 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class ProoductFactoryTest {
     public static void main(String[] args) {
+        //使用工厂类获取实例类
         ApplicationContext applicationContext = new ClassPathXmlApplicationContext("bean.xml");
         Product product = (Product) applicationContext.getBean("ProductFactory");
         System.out.println(product.toString());
+        //获取工厂类，需要使用&转义符
         ProductFactory factory = (ProductFactory) applicationContext.getBean("&ProductFactory");
         System.out.println(factory.isSingleton());
     }
