e8f25e541cc0dad11c407d65a5fceca5d8cbca24
lkyi <269577519@qq.com>
2020-08-17 21:25:15 +0800

    mac首次提交

--- Part-8-Spring-transaction/src/main/java/com/lkyi/pojo/User.java
+++ Part-8-Spring-transaction/src/main/java/com/lkyi/pojo/User.java
@@ -11,4 +11,10 @@ public class User {
     private Integer id;
     private String name;
     private Integer age;
+
+    public User(Integer id, String name, Integer age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
 }
